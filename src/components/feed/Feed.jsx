c0da11c7c8996fcdf785e615c31610d24be1e5bb
codingwithRaja <rjali40@gmail.com>
2023-05-20 00:12:03 +0500

    done

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./feed.css"
+const Feed = () => {
+    return (
+        <div>This is feed</div>
+    )
+}
+
+export default Feed
---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./rightbar.css"
+
+const Rightbar = () => {
+    return (
+        <div>This is Rightbar</div>
+    )
+}
+
+export default Rightbar
