d24c0af9c7ac8503999be4356f4c8a8bd34ffd82
codingwithRaja <rjali40@gmail.com>
2023-05-19 23:58:31 +0500

    Topbar Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import Home from "./pages/home/Home";
 
 
 
 function App() {
   return (
     <>
-      <h1>XeonBook Roboto</h1>
-      <p> This is Paragraph</p>
+      <Home />
+
     </>
   );
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+// import "home.css"
+const Home = () => {
+    return (
+        <Topbar />
+    )
+}
+
+export default Home
